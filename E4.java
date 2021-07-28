d7d34462379f7dd19fba3e1a4edbd33e43be1a22
Nadia <nbla.escom@gmail.com>
2021-07-27 20:03:56 -0500

    Exercise 4 updated

--- E4.java
+++ E4.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class E4 {
 
     public static boolean isPermutationP(String cad){
@@ -17,11 +19,39 @@ public class E4 {
             }
         }
         return true;
-    }    
+    }
+    
+    
+    public static int getCharNumber(char c){
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(c);
+        if( a <= val && val <= z){
+            return val - a;
+        }
+        return -1;
+    }
+    
+    public static boolean isPermutationP2(String cad){  
+        int countOdd = 0;
+        int[] table = new int[Character.getNumericValue('z')- Character.getNumericValue('a') + 1];
 
+        for(char c : cad.toCharArray()){
+            int x = getCharNumber(c);
+               if( x != -1){
+                table[x]++;
+                if(table[x] % 2 == 1){
+                    countOdd++;
+                }else{
+                    countOdd--;
+                }
+            }
+        }
+        return countOdd <= 1;
+    }
 
     public static void main(String[] args){
-        if(isPermutationP("hann")){
+        if(isPermutationP2("tact")){
             System.out.println("La cadena si forma un palidromo");
         }else{
             System.out.println("La cadena no forma un palidromo");
