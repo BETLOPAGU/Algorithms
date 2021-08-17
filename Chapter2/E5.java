2dd352883a655a745a4cab67d0421f07025c70bf
Nadia <nbla.escom@gmail.com>
2021-08-17 16:56:20 -0500

    Exercise 5 done, pending for generic solution.

--- Chapter2/E5.java
+++ Chapter2/E5.java
@@ -1,17 +1,38 @@
 import java.util.*; 
 
 public class E5<T> extends Lista<T> {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> HT = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            int num_complement = target - nums[i];
-            if (HT.containsKey(num_complement)) {
-                return new int[] { HT.get(num_complement), i };
-            } else {
-                HT.put(nums[i], i);
+    public Node<T> addTwoNumbers(Node<T> l1, Node<T> l2) {
+        Node<T> res = null;
+        Node<T> prev = null;
+        Node<T> temp = null;
+        int carry = 0, sum;
+        while (l1 != null || l2 != null) {
+            sum = carry + l1 != null ? l1.data : 0 + l2 != null ? l2.data : 0;
+            
+            carry = (sum >= 10) ? 1 : 0;
+            sum = sum % 10;
+            temp.data = sum;
+
+            if (res == null) {
+                res = temp;
+            }else {
+                prev.next = temp;
+            }
+            
+            prev = temp;
+            
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
             }
         }
-        return new int[] {};
+ 
+        if (carry > 0) {
+            Node<T> other = temp.next;
+            other.data = carry;
+        }
+        return res;
     }
 }
